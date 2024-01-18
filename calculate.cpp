df84fd3855e7fd1da04ac070768097ca572e7657
sanjay <sanjaylashkarmaa1baba1@gmail.com>
2024-01-18 21:46:10 +0530

    new

---
+++ calculate.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+ using namespace std;
+
+ int main(){
+
+    int a,b,c;
+    cout<<"enter two number";
+    cin>>a>>b;
+    c=a/b;
+    cout<<"total is "<<c;
+    return 0;
+
+
+ }
